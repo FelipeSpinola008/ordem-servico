08239c48180e41d0405991bebbfe037da8f92cb6
FelipeSpinola008 <felipespinola784@gmail.com>
2025-12-19 13:14:52 -0300

    :rocket: feat: implement real database insertion for service orders

---
+++ scripts/test-create-order.js
@@ -0,0 +1,16 @@
+const { registerOrder } = require('../src/controllers/OrderController');
+
+async function runtTest() {
+    try {
+        console.log('🚀 Tentando criar uma ordem de teste...');
+        const newOrder = await registerOrder('Felipe cappuccino', 'troca de teclado mecânico');
+        console.log('📋 Dados retornados pelo Banco:')
+        console.table(newOrder);
+
+        process.exit(0)
+    } catch (err) {
+        console.error('💀 O teste falhou:', err.message);
+        process.exit(1);
+    }    
+}
+runtTest();
---
+++ src/controllers/OrderController.js
@@ -0,0 +1,28 @@
+const pool = require('../config/db');
+
+/**
+ * Registra uma nova Ordem de Serviço no banco de dados.
+ * @param {string} customer - Nome do cliente.
+ * @param {string} description - Detalhes do serviço.
+ */
+
+const registerOrder = async (customer, description) => {
+    const query = `INSERT INTO service_orders (customer_name, description)
+    VALUES ($1, $2)
+    RETURNING *;
+    `;
+    const values = [customer, description];
+
+    try {
+        const res = await pool.query(query, values);
+    
+        console.log('✅ Ordem salva no banco com sucesso!')
+        return res.rows[0];
+    } catch (err) {
+        console.error('❌ Erro no Controller ao salvar:', err.message);
+        throw err;
+    }
+};
+
+module.exports = { registerOrder }
+
