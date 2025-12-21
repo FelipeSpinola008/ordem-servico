2ae68bc5ee6c3ae1a2628f28dcda6f72588871c2
FelipeSpinola008 <felipespinola784@gmail.com>
2025-12-20 21:51:57 -0300

    🌐🔥 feat: implement app.js initialize express server and connect order routes

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const orderRoutes = require('./routes/OrderRoutes');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/orders', orderRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando tranquilamente na porta ${PORT}`);
+    console.log(`🔗 Teste a listagem em: http://localhost:${PORT}/api/orders`);
+});
+
+module.exports = app;
