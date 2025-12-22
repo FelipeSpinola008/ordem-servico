82f0fd3d2369ad8b868a0b0ce64437f565a3e104
FelipeSpinola008 <felipespinola784@gmail.com>
2025-12-22 15:52:10 -0300

    :sparkles: feat: add a login structure using JWT in UserController.js.

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,5 +1,6 @@
 const pool = require('../config/db');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userController = {
     register: async (req, res) => {
@@ -25,6 +26,29 @@ const userController = {
         } catch (error) {
             console.error(error);
             res.status(500).json({ error: 'Erro ao cadastrar usuário.' });
+        }
+    },
+    login: async (req, res) => {
+        try {
+            const { email, password } = req.body;
+        const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (user.rows.length === 0) {
+            return res.status(401).json({ error: 'credenciais inválidas.'})
+        }
+
+        const validPassword = await bcrypt.compare(password, user.rows[0].password_hash);
+        if (!validPassword) {
+            return res.status(401).json({ error: 'Credenciais inválidas.' });
+        }
+
+        const token = jwt.sign(
+            { id: user.rows[0].role },
+            process.env.JWT_SECRET,
+            { expiresIn: '1d' }
+        );
+        res.json( {message: 'login realizado com sucesso', token})
+        } catch (error) {   
+            res.status(500).json({ error: 'Erro no login.' });
         }
     }
 }
