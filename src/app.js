d8133d665fbb307232f84a7961423d2a9a300956
FelipeSpinola008 <felipespinola784@gmail.com>
2025-12-23 00:53:58 -0300

    🛡️☕ chore: add CORS support and stabilize backend for frontend integration

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-require('dotenv').config();
 const express = require('express');
+const cors = require('cors')
+require('dotenv').config();
 const orderRoutes = require('./routes/OrderRoutes');
 const authRoutes = require('./routes/AuthRoutes');
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.static('public'));
 
